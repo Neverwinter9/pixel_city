4db2a874dcd9b02576b7704936699fa27b995a27
Neverwinter9 <neverwinter99@gmail.com>
2019-01-18 14:09:44 -0800

    First commit: Created MapVC

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,57 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Pavel Ponomarev on 1/17/19.
+//  Copyright © 2019 Pavel Ponomarev. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // Variables
+    var locationManager = CLLocationManager()
+    let authStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+    }
+
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+}
+
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
